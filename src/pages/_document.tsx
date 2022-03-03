50cfba19384f4b3a1e567f4d0eef735b355497b6
Josue Amorim <josuedesouzaamorim@gmail.com>
2022-03-03 09:53:11 -0300

    Fonts from google was added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,11 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>IgNews</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
