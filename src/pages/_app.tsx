c69d70505a832c88c51c58bdeed379f845d12d8b
Josue Amorim <josuedesouzaamorim@gmail.com>
2022-05-11 18:32:59 -0300

    Added sessionProvider from next-auth and Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import { AppProps } from 'next/app'
+import '../styles/global.scss'
+import { Header } from '../components/Header'
+import { SessionProvider } from 'next-auth/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps }
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Header />
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
-
-export default MyApp
