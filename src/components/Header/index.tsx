59a256e6401745747d8193d84282054ef8bea6ca
Josue Amorim <josuedesouzaamorim@gmail.com>
2022-05-15 13:06:17 -0300

    Added nextLinks and title of page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,32 @@
 import styles from '../../styles/Header.module.scss'
 import { SignInButton } from './SignInButton'
+import Link from 'next/link'
+import Head from 'next/head'
 
 export function Header() {
   return (
-    <div className={styles.Container}>
-      <div className={styles.Content}>
-        <img src="logo.svg" alt="IgNews" />
-        <nav>
-          <ul>
-            <li className={styles.active}>Home</li>
-            <li>Posts</li>
-          </ul>
-        </nav>
+    <>
+      <Head>
+        <title>ignews | Posts</title>
+      </Head>
 
-        <SignInButton />
+      <div className={styles.Container}>
+        <div className={styles.Content}>
+          <img src="logo.svg" alt="IgNews" />
+          <nav>
+            <ul>
+              <li className={styles.active}>
+                <Link href="http://localhost:3000">Home</Link>
+              </li>
+              <li>
+                <Link href="http://localhost:3000/posts">Posts</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <SignInButton />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
