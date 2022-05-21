13ccfae222e318aa059b0572af94f1938577e49b
Josue Amorim <josuedesouzaamorim@gmail.com>
2022-05-21 18:04:35 -0300

    added subscription status check

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth from 'next-auth'
+import NextAuth, { Session } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import { fauna } from '../../../services/fauna'
 import { query as q } from 'faunadb'
@@ -16,6 +16,42 @@ export default NextAuth({
     })
   ],
   callbacks: {
+    async session({session}){
+    try {
+      const userActiveSubscription = await fauna.query(
+        q.Get(
+       q.Intersection(
+         [
+          q.Match(
+            q.Index('subscription_by_user_ref'),
+            q.Select(
+              "ref",
+              q.Get(
+                q.Match(
+                  q.Index('user_by_email'),  
+                  q.Casefold(session.user.email)
+                )
+              )
+            )
+          ), q.Match(
+            q.Index('subscription_by_status'), "active"
+          )
+         ]
+       )
+      )
+      )
+      return {
+        ...session,
+        userActiveSubscription: userActiveSubscription
+      }
+    } catch {
+      return {
+        ...session,
+        userActiveSubscription: null
+      }
+    }
+      
+    },
     async signIn({ user, account, profile }) {
       try {
         await fauna.query(
