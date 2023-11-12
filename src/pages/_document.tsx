0450cedddef263936d8af55c9c46f9e0499423ce
Josué Amorim <josuedesouzaamorim@gmail.com>
2023-11-12 15:58:46 -0300

    Validation

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+        <meta name="impact-site-verification" content="cf59db74-1730-445d-b40a-9ebac03fdf11" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;900&family=Roboto:wght@400;700&display=swap"
